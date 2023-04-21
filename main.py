4da71013ef2c09597f49ca490b3c535556f8c769
Jhv <jimenezjheave123@gmail.com>
2023-04-21 15:19:24 +0800

    add comment

--- main.py
+++ main.py
@@ -10,10 +10,23 @@ grid = [
     [30, 39, 21, 32, None, None],
 ]
 
+"""
+ A lambda function is defined to extract the set of numbers from a list of cells. The filter function is used to remove 
+ None values from the input list, and the resulting list is then converted to a set.
+"""
 get_numbers = lambda cells: set(filter(lambda cell: cell is not None, cells))
 
+"""
+ A lambda function finds the set of numbers that are missing from a list of cells.
+ It first generates a set containing all numbers between 1 and 50 using the range function,
+ and then subtracts the set of numbers that are present in the input list by calling the get_numbers lambda function defined earlier.
+"""
 find_missing_numbers = lambda cells: set(range(1, 51)) - get_numbers(cells)
 
+"""
+ A nested list comprehension to compute the list of possible missing numbers for each cell in the grid. 
+ For each row and column index (row, col) in the grid, it generates a list of all the numbers in the corresponding row, column, and diagonals
+"""
 missing_numbers = [[find_missing_numbers(list(chain(
     grid[row],
     [grid[i][col] for i in range(len(grid))],
